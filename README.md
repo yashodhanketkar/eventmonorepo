2e702207956cc71e0da7cc64a96334964eedf6e7
Yashodhan Ketkar <kykyashodhan@gmail.com>
2023-12-01 01:10:49 +0530

    fixes and new files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Event Manager
+
+A simple full stack application to add, edit, and delete events.
+
+## Tech Stack
+
+- React
+- Express
+- MongoDB
+- Firebase
+- Redux Toolkit
+
+### Licence
+
+MIT © 2023
--- client/src/components/interface/footer.tsx
+++ client/src/components/interface/footer.tsx
@@ -3,7 +3,14 @@ export const Footer = () => {
   return (
     <footer className="grid w-full grid-cols-2 px-4 py-2 bg-black/5">
       <div className="text-left">Events</div>
-      <div className="text-right">Github</div>
+      <div className="text-right">
+        <a
+          href="https://github.com/yashodhanketkar/eventmonorepo"
+          target="_blank"
+        >
+          Github
+        </a>
+      </div>
       <div className="col-span-2 text-xs text-center">
         ©{currentYear !== 2023 && "2023-"}
         {currentYear} Events. All rights reserved
